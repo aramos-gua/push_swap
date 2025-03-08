41c00aa7304ef2f270cfafc4679ce19ca57ea7c4
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-03-08 10:02:21 +0000

    added split_argv functional, tested

--- push_swap.c
+++ push_swap.c
@@ -1,19 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <alejandro.ramos.gua@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 14:10:30 by aramos            #+#    #+#             */
+/*   Updated: 2025/03/07 14:10:31 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h" 
 
-int	validate_input(char *str)
+int	validate_input(char **argv)
 {
-	while (*str)
+	char	*invalid;
+	int	i;
+
+	i = 1;
+	invalid = "Error";
+	while (argv[i])
 	{
-		if (*str == '-')
+		if (argv[i] == '-')
 		{
-			if (*(str - 1) != ' ')
-				return (1);
-			else if (!(ft_isdigit(*(str + 1))))
-				return (1);
+			if (!(ft_isdigit(argv[i + 1]))
+				ft_printf("%s", invalid);
 		}
-		else if (!(ft_isdigit(*str) || *str == ' '))
-			return (1);
-		str++;
+		else if (!(ft_isdigit(argv[i])))
+			ft_printf("%s", invalid);
+		i++;
 	}
 	return (0);
 }
@@ -23,8 +38,9 @@ int	main(int argc, char **argv)
 	char	*invalid;
 
 	invalid = "Error";
-	if (argc < 2)
+	if (argc < 2 || (argc == 2 && argv[1][0] == '\0'))
 		ft_printf("%s\n", invalid);
-	else if (argc == 2 && (validate_input(argv[1])))
-		ft_printf("%s\n", invalid);
+	else if (argc == 2)
+		argv = split_argv(argv[1], ' ');
+	validate_input(argv);
 }
---
+++ split_argv.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_argv.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <alejandro.ramos.gua@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 14:10:18 by aramos            #+#    #+#             */
+/*   Updated: 2025/03/07 17:00:24 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	word_count(char *str, char c)
+{
+	int	words;
+	
+	words = 0;
+	while (*str && *str == c)
+		str++;
+	while (*str)
+	{
+		words++;
+		while (*str && *str != c)
+			str++;
+		while (*str && *str == c)
+			str++;
+	}
+	return (words);
+}
+
+char	*fill_words(char *str, char c)
+{
+	static int	start_w = 0;
+	int			len_word;
+	char		*word;
+	int			i;
+
+	i = 0;
+	len_word = 0;
+	while (str[start_w] && str[start_w] == c)
+		start_w++;
+	while (str[start_w + len_word] && str[start_w + len_word] != c)
+		++len_word;
+	word = malloc((len_word + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (str[start_w] && str[start_w] != c)
+		word[i++] = str[start_w++];
+	word[i] = '\0';
+	return (word);
+}
+
+char	**split_argv(char *str, char c)
+{
+	int		i;
+	int		words;
+	char	**array;
+
+	i = 0;
+	words = word_count(str, c);
+	array = malloc((words + 1) * sizeof(char *));
+	if (!array)
+		return (NULL);
+	while (words-- > 0)
+	{
+		array[i++] = fill_words(str, c);
+	}
+	array[i] = NULL;
+	return (array);
+}
